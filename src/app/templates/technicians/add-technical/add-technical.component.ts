412c10bd1f5bc968d82524414d41ea07b33e9943
islamTech <islam_fe@inspia.net>
2020-09-22 14:33:15 +0200

    edit add technical ts

--- src/app/templates/technicians/add-technical/add-technical.component.ts
+++ src/app/templates/technicians/add-technical/add-technical.component.ts
@@ -405,7 +405,7 @@ export class AddTechnicalComponent implements OnInit {
 
       service_tech: data['services'].map(el => el.id),
       city_tech: data['cities'].map(el => el.id),
-      contract_type : data['contract'].id ,
+      contract_type : data['contract'] ? data['contract'].id : null ,
 
       serviceObj: data['service'],
       cityObj: data['city'],
