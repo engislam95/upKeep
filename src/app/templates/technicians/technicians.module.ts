60fe8f9f0a2ffd7c9e29ff921cf1ef0fec00e184
MohammedMahmoudAbdelkarim <mohammedmahmoudabdelkarim@gmail.com>
2020-11-02 13:39:08 +0200

    add tech and invoices report

--- src/app/templates/technicians/technicians.module.ts
+++ src/app/templates/technicians/technicians.module.ts
@@ -1,19 +1,28 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { SharedComponentsModule } from 'src/app/components/shared-components.module';
-import { ModalModule } from 'ngx-bootstrap/modal';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { SharedComponentsModule } from "src/app/components/shared-components.module";
+import { ModalModule } from "ngx-bootstrap/modal";
 
-import { TechniciansRoutingModule } from './technicians-routing.module';
-import { MatTableModule, MatInputModule, MatSlideToggleModule } from '@angular/material';
-import { AddTechnicalComponent } from './add-technical/add-technical.component';
-import { AllTechniciansComponent } from './all-technicians/all-technicians.component';
-import { TechnicalDetailsComponent } from './technical-details/technical-details.component';
-import { ToolsModule } from 'src/app/tools/tools.module';
-import { RatingModule } from 'ngx-bootstrap/rating';
-import { BarRatingModule } from 'ngx-bar-rating-custom/rating.module';
+import { TechniciansRoutingModule } from "./technicians-routing.module";
+import {
+  MatTableModule,
+  MatInputModule,
+  MatSlideToggleModule,
+  MatCheckboxModule,
+} from "@angular/material";
+import { AddTechnicalComponent } from "./add-technical/add-technical.component";
+import { AllTechniciansComponent } from "./all-technicians/all-technicians.component";
+import { TechnicalDetailsComponent } from "./technical-details/technical-details.component";
+import { ToolsModule } from "src/app/tools/tools.module";
+import { RatingModule } from "ngx-bootstrap/rating";
+import { BarRatingModule } from "ngx-bar-rating-custom/rating.module";
 
 @NgModule({
-  declarations: [AddTechnicalComponent, AllTechniciansComponent, TechnicalDetailsComponent],
+  declarations: [
+    AddTechnicalComponent,
+    AllTechniciansComponent,
+    TechnicalDetailsComponent,
+  ],
   imports: [
     //
     CommonModule,
@@ -21,11 +30,12 @@ import { BarRatingModule } from 'ngx-bar-rating-custom/rating.module';
     SharedComponentsModule,
     ToolsModule,
     MatInputModule,
+    MatCheckboxModule,
     MatTableModule,
-    MatSlideToggleModule ,
-    BarRatingModule ,
-    RatingModule.forRoot() ,
-    ModalModule.forRoot()
-  ]
+    MatSlideToggleModule,
+    BarRatingModule,
+    RatingModule.forRoot(),
+    ModalModule.forRoot(),
+  ],
 })
 export class TechniciansModule {}
