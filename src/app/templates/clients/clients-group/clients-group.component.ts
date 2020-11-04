19fbba9eff80b3a313f9ca84c03adc926cb5fb31
MohammedMahmoudAbdelkarim <mohammedmahmoudabdelkarim@gmail.com>
2020-11-04 16:16:37 +0200

    upload reports

--- src/app/templates/clients/clients-group/clients-group.component.ts
+++ src/app/templates/clients/clients-group/clients-group.component.ts
@@ -22,21 +22,6 @@ export class ClientsGroupComponent implements OnInit {
   arr = [];
   ngOnInit() {}
 
-  dataURLtoFile(dataurl, filename) {
-    console.log(dataurl);
-
-    var arr = dataurl.split(","),
-      mime = arr[0].match(/:(.*?);/)[1],
-      bstr = atob(arr[1]),
-      n = bstr.length,
-      u8arr = new Uint8Array(n);
-
-    while (n--) {
-      u8arr[n] = bstr.charCodeAt(n);
-    }
-
-    return new File([u8arr], filename, { type: mime });
-  }
   // Upload Excel File
   updateExcel(event) {
     console.log(event);
